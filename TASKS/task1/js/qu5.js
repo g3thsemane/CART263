324db1f470a1c8b2f98aa510e9ef5233a7b00555
g3thsemane <saltyravensfan@gmail.com>
2026-01-15 21:47:45 -0500

    continuation

--- TASKS/task1/js/qu5.js
+++ TASKS/task1/js/qu5.js
@@ -1,11 +1,48 @@
 "use strict";
 
-function setup() {
+let counter = 0;
 
+let square1 = {
+
+    x: 50,
+    y: 50,
+    size: 50,
+    color: [200, 30, 2]
+}
+
+function setup() {
+    createCanvas(750, 750);
 }
 
 function draw() {
+    background(100, 0, 100);
 
+    if (mouseSquareOverlap()) {
+        square1.color = [225, 65, 20];
+    } else {
+        square1.color = [200, 30, 2];
+    }
+
+    drawSquare();
 
 }
 
+function drawSquare() {
+
+    rect(square1.x, square1.y, square1.size, square1.size);
+    fill(square1.color);
+    noStroke();
+}
+
+function mouseSquareOverlap() {
+
+    return (mouseX >= square1.x && mouseX <= square1.x + square1.size && mouseY >= square1.y && mouseY <= square1.y + square1.size);
+}
+
+function mousePressed() {
+
+    if (mouseSquareOverlap()) {
+        counter += 1;
+        console.log(counter);
+    }
+}
