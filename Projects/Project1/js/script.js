16cf28622f22a4c9af65e5e230351a5c78a15382
g3thsemane <saltyravensfan@gmail.com>
2026-02-28 19:10:59 -0500

    began working on script

---
+++ Projects/Project1/js/script.js
@@ -0,0 +1,16 @@
+/* 
+* CART 263 A
+* Project 1: You Can Run And You Can Hide But You Can't Run And You Can't Hide
+*
+* Benjamin Merhi
+*
+* A game based on the invasive nature of pop up advertisements.
+*
+*/
+
+//Configuration variables
+const gameW = 900;
+const gameH = 600;
+
+const playerSize = 18;
+const playerSpeed = 260;
